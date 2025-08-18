b46703806d0093503cfe13a40eaabbe3d253f9d0
Abinay-Brown <abinay.joel@gmail.com>
2025-08-18 12:13:01 -0400

    Tested Constraints

--- iSLS_Optimizer.py
+++ iSLS_Optimizer.py
@@ -52,7 +52,7 @@ class SLS_optimizer:
         
         # Set b for bounds
         self.bx.value = np.array([[xlim, -xlim], [xlim, -xlim], [vlim, -vlim], [vlim, -vlim]])
-        self.bu.value = np.array([[ulim, -ulim], [ulim, -ulim], [ulim, -ulim]])
+        self.bu.value = np.array([[ulim, -ulim], [ulim, -ulim]])
         
         return True
     def initDecisionVariables(self):
@@ -71,64 +71,69 @@ class SLS_optimizer:
         for i_blk in range(N):        
             for j_blk in range(N):    
                 if j_blk > i_blk:     
-                    self.constraints += [Phi_x[i_blk*nx:(i_blk+1)*nx, j_blk*nx:(j_blk+1)*nx] == 0]
-                    self.constraints += [Phi_u[i_blk*nu:(i_blk+1)*nu, j_blk*nx:(j_blk+1)*nx] == 0]
+                    self.Constraints += [self.Phi_x[i_blk*nx:(i_blk+1)*nx, j_blk*nx:(j_blk+1)*nx] == 0]
+                    self.Constraints += [self.Phi_u[i_blk*nu:(i_blk+1)*nu, j_blk*nx:(j_blk+1)*nx] == 0]
     
                     
         # [I - ZA - ZB]*Phi = [I] Constraint
-        Phi = cp.vstack([self.Phi_x, self.Phi_u])
-        self.Constraints += [(self.Iblk - self.Zblk@self.Ablk - self.Zblk@self.Bblk)@Phi == self.Iblk]
+        #print(P)
+        #Phi = cp.vstack([self.Phi_x, self.Phi_u])
+        #print(Phi.shape)
+        #print(((self.Iblk - self.Zblk@self.Ablk - self.Zblk@self.Bblk)@Phi).shape)
+        
+        self.Constraints += [((self.Iblk - self.Zblk@self.Ablk)@self.Phi_x - (self.Zblk@self.Bblk@self.Phi_u)) == self.Iblk]
         
         # State Tube Constraint
         # Loop through the Trajectory points
-        for i in range(NUM):
+        for i in range(N):
             # Loop through each state
             for j in range(nx):
                 # Loop to index block matrices
                 LHS_upper = 0
                 LHS_lower = 0
                 for k in range(i+1):
-                    LHS_upper = LHS_upper + cp.norm(self.cx[j, :]@Phi_x[i*nx: (i+1)*nx, k*nx: (k+1)*nx]@cp.hstack(self.E, self.mu*self.tau[k]**2), 'inf')
-                    LHS_lower = LHS_lower + cp.norm(-self.cx[j, :]@Phi_x[i*nx: (i+1)*nx, k*nx: (k+1)*nx]@cp.hstack(self.E, self.mu*self.tau[k]**2), 'inf')               
+                    LHS_upper = LHS_upper + cp.norm(self.cx[j, :]@self.Phi_x[i*nx: (i+1)*nx, k*nx: (k+1)*nx]@cp.hstack([self.E, self.mu*self.tau[k]**2]), 'inf')
+                    LHS_lower = LHS_lower + cp.norm(-self.cx[j, :]@self.Phi_x[i*nx: (i+1)*nx, k*nx: (k+1)*nx]@cp.hstack([self.E, self.mu*self.tau[k]**2]), 'inf')               
                 # Set Upper Tube Bound
                 LHS_upper = LHS_upper + self.cx[j, :]@self.z[:, i] + self.bx[j, 0]
                 # Set Lower Tube Bound  
                 LHS_lower = LHS_lower - self.cx[j, :]@self.z[:, i] + self.bx[j, 1]
                 self.Constraints += [LHS_upper <= 0]
                 self.Constraints += [LHS_lower <= 0]
-
+        
         # Control Tube Contraint
         # Loop through the Trajectory points
-        for i in range(NUM):
+        for i in range(N):
             # Loop through each state
             for j in range(nu):
                 # Loop to index block matrices
                 LHS_upper = 0
                 LHS_lower = 0
                 for k in range(i+1):
-                    LHS_upper = LHS_upper + cp.norm(self.cu[j, :]@Phi_u[i*nu: (i+1)*nu, k*nx: (k+1)*nx]@cp.hstack(self.E, self.mu*self.tau[k]**2), 'inf')
-                    LHS_lower = LHS_lower + cp.norm(-self.cu[j, :]@Phi_u[i*nu: (i+1)*nu, k*nx: (k+1)*nx]@cp.hstack(self.E, self.mu*self.tau[k]**2), 'inf')               
+                    LHS_upper = LHS_upper + cp.norm(self.cu[j, :]@self.Phi_u[i*nu: (i+1)*nu, k*nx: (k+1)*nx]@cp.hstack([self.E, self.mu*self.tau[k]**2]), 'inf')
+                    LHS_lower = LHS_lower + cp.norm(-self.cu[j, :]@self.Phi_u[i*nu: (i+1)*nu, k*nx: (k+1)*nx]@cp.hstack([self.E, self.mu*self.tau[k]**2]), 'inf')               
                 # Set Upper Tube Bound
                 LHS_upper = LHS_upper + self.cu[j, :]@self.v[:, i] + self.bu[j, 0]
                 # Set Lower Tube Bound  
                 LHS_lower = LHS_lower - self.cu[j, :]@self.v[:, i] + self.bu[j, 1]
                 self.Constraints += [LHS_upper <= 0]
                 self.Constraints += [LHS_lower <= 0]
-
+        
         # 27e constraint
-        for i in range(1, NUM):
+        for i in range(1, N):
             LHS = 0
             for k in range(i):
-                LHS = LHS + cp.norm(Phi_x[(i-1)*nx: i*nx, k*nx: (k+1)*nx]@cp.hstack(self.E, self.mu*self.tau[k]**2), 'inf')               
+                LHS = LHS + cp.norm(self.Phi_x[(i-1)*nx: i*nx, k*nx: (k+1)*nx]@cp.hstack([self.E, self.mu*self.tau[k]**2]), 'inf')               
             self.Constraints += [LHS_upper <= self.tau[i]]
         
-        for i in range(1, NUM):
+        for i in range(1, N):
             LHS = 0
             for k in range(i):
-                LHS = LHS + cp.norm(Phi_u[(i-1)*nu: i*nu, k*nx: (k+1)*nx]@cp.hstack(self.E, self.mu*self.tau[k]**2), 'inf')               
+                LHS = LHS + cp.norm(self.Phi_u[(i-1)*nu: i*nu, k*nx: (k+1)*nx]@cp.hstack([self.E, self.mu*self.tau[k]**2]), 'inf')               
             self.Constraints += [LHS <= self.tau[i]]
             
-        return
+        return True
+        
     def computeLinearizationError():
         mu = 0
         return mu
@@ -150,9 +155,15 @@ class SLS_optimizer:
         return Ablk, Bblk, Zblk
     
 Nominal = scipy.io.loadmat('Guess_Trajectory.mat')
-Xnom = Nominal['Xnom'].T
-Unom = Nominal['Unom'].T
+Xnom = Nominal['Xnom']
+Unom = Nominal['Unom']
 Tnom = Nominal['Tnom'].item()    
+Xnom = Xnom[:, 0:-1]
+
 opt = SLS_optimizer()
-Ablk, Bblk, Zblk = opt.Linearize(Xnom, Unom, Tnom)
+opt.initParameters()
+opt.setParameters(Xnom, Unom, Tnom, np.diag([0, 0, 0, 0]))
+opt.initDecisionVariables()
+opt.initConstraints()
+
  
